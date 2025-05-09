429bf50eeceaef5583f8433540951e192e81bca6
keshavsoft <nknnkumar@yahoo.com>
2025-05-09 11:19:25 +0530

    KeshavSoft

---
+++ FileAsArray/routes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const router = express.Router();
+
+import { router as routerFromCreate } from "./Create/routes.js";
+
+router.use("/Create", routerFromCreate);
+
+export { router };
