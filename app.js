6a41ca12fbadd238e1af90037ba65cff706352c8
varshitha-2319 <163839907+varshitha-2319@users.noreply.github.com>
2025-05-12 18:24:06 +0530

    FileArray

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 const app = express()
 const port = 3000;
 
+import { router as routerFromVarshitha } from "./Varshitha/routes.js";
 import { router as routerFromSharath } from "./Sharath/routes.js";
 import { router as routerFromFileAsArray } from "./FileAsArray/routes.js";
 import { router as routerFromContentInArray } from "./ContentInArray/routes.js";
@@ -12,6 +13,7 @@ app.use(express.static('public'));
 app.use('/FileAsArray', routerFromFileAsArray);
 app.use('/ContentInArray', routerFromContentInArray);
 app.use('/V1', routerFromV1);
+app.use("/Varshitha", routerFromVarshitha);
 app.use("/Sharath", routerFromSharath);
 
 app.listen(port, () => {
