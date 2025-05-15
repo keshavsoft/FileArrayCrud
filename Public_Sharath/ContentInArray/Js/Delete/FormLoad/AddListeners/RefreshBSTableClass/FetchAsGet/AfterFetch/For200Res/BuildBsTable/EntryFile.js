d2439fa667b408d816765f9946b550e3fb86d462
sharath0036 <ksharathbabu2@gmail.com>
2025-05-15 10:55:33 +0530

    Ui for delete and ContentInArray

---
+++ Public_Sharath/ContentInArray/Js/Delete/FormLoad/AddListeners/RefreshBSTableClass/FetchAsGet/AfterFetch/For200Res/BuildBsTable/EntryFile.js
@@ -0,0 +1,40 @@
+import optionsJson from './options.json' with { type: 'json' };
+import { StartFunc as ForColumns } from "./ForColumns/EntryFile.js";
+import { StartFunc as StartFuncOnClickRowFunc } from "./onClickRow/EntryFile.js";
+
+const $table = $('#table');
+
+const StartFunc = ({ inResponseAsJson }) => {
+    optionsJson.data = inResponseAsJson;
+    optionsJson.onClickRow = StartFuncOnClickRowFunc;
+
+    ForColumns({ inColumns: optionsJson.columns });
+    jFLocalPrepareColumns({ inFirstRow: inResponseAsJson[0] });
+
+    $table.bootstrapTable(optionsJson);
+};
+const jFLocalPrepareColumns = ({ inFirstRow }) => {
+    const excludedKeys = ["UuId", "DateTime"];
+    const keyMappings = { pk: "Dc" };
+
+    Object.keys(inFirstRow).forEach((inKey) => {
+        if (!excludedKeys.includes(inKey)) {
+            optionsJson.columns.push({
+                field: inKey,
+                title: keyMappings[inKey] || inKey
+            });
+        }
+    });
+    optionsJson.columns.push({
+        field: "Delete",
+        title: "Delete",
+        align: "center",
+        formatter: function (value, row, index) {
+            return `
+            <i class="bi bi-trash text-danger me-2" style="cursor:pointer;" title="Edit"></i>`;
+        }
+    });
+
+};
+
+export { StartFunc };
