3275ee9403b7a5e9316e626b3364003ee76cf0a8
keshavsoft <nknnkumar@yahoo.com>
2025-05-15 13:04:53 +0530

    V4 started

--- V4/routes.js
+++ V4/routes.js
@@ -2,4 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
+import { router as routerFromSharath } from "./Sharath/routes.js";
+import { router as routerFromKeshav } from "./Keshav/routes.js";
+
+router.use("/Sharath", routerFromSharath);
+router.use("/Keshav", routerFromKeshav);
+
 export { router };
--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import { router as routerFromV1 } from "./V1/routes.js";
 import { router as routerFromV3 } from "./V3/routes.js";
 import { router as routerFromLogin } from "./Login/routes.js";
 import { router as routerFromMiddleWares } from "./MiddleWares/entryFile.js";
+import { router as routerFromV4 } from "./V4/routes.js";
 
 app.use(express.static('Public'));
 
@@ -16,6 +17,7 @@ app.use('/ContentInArray', routerFromContentInArray);
 app.use('/V1', routerFromV1);
 app.use('/V3', routerFromV3);
 app.use('/Login', routerFromLogin);
+app.use("/V4", routerFromV4);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
