ef17ff676cd22510b56267cbf2ebeb0fc93060b4
Srivasu215 <sri.vasu215@gmail.com>
2025-05-15 15:26:55 +0530

    Sree

--- V4/routes.js
+++ V4/routes.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 const router = express.Router();
 
+import { router as routerFromSree } from "./Sree/routes.js";
 import { router as routerFromSharath } from "./Sharath/routes.js";
 import { router as routerFromKeshav } from "./Keshav/routes.js";
 
+router.use("/Sree", routerFromSree);
 router.use("/Sharath", routerFromSharath);
 router.use("/Keshav", routerFromKeshav);
 
