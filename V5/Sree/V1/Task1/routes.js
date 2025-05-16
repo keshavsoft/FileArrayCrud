6192bde1d32b571eb79baafe770bdb65b3e2a86e
Srivasu215 <sri.vasu215@gmail.com>
2025-05-16 13:46:49 +0530

    V5 Sree InsertWithPkAndUuIdAndDateTime started

--- V5/Sree/V1/Task1/routes.js
+++ V5/Sree/V1/Task1/routes.js
@@ -2,12 +2,14 @@ import express from 'express';
 
 const router = express.Router();
 
+import { router as routerFromInsertWithPkAndUuIdAndDateTime } from "./InsertWithPkAndUuIdAndDateTime/routes.js";
 import { router as routerFromRowDataWithPk } from "./RowDataWithPk/routes.js";
 import { router as routerFromDelete } from "./Delete/routes.js";
 import { router as routerFromAlter } from "./Alter/routes.js";
 import { router as routerFromInsertWithPk } from "./InsertWithPk/routes.js";
 import { router as routerFromShowAll } from "./ShowAll/routes.js";
 
+router.use("/InsertWithPkAndUuIdAndDateTime", routerFromInsertWithPkAndUuIdAndDateTime);
 router.use("/RowDataWithPk", routerFromRowDataWithPk);
 router.use("/Delete", routerFromDelete);
 router.use("/Alter", routerFromAlter);
