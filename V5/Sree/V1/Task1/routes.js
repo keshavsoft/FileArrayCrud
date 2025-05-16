c4dcc0894d0f2387947ff13ce01f1a8daa742597
Srivasu215 <sri.vasu215@gmail.com>
2025-05-16 11:46:42 +0530

    Sree

---
+++ V5/Sree/V1/Task1/routes.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const router = express.Router();
+
+import { router as routerFromRowDataWithPk } from "./RowDataWithPk/routes.js";
+import { router as routerFromDelete } from "./Delete/routes.js";
+import { router as routerFromAlter } from "./Alter/routes.js";
+import { router as routerFromInsertWithPk } from "./InsertWithPk/routes.js";
+import { router as routerFromShowAll } from "./ShowAll/routes.js";
+
+router.use("/RowDataWithPk", routerFromRowDataWithPk);
+router.use("/Delete", routerFromDelete);
+router.use("/Alter", routerFromAlter);
+router.use("/InsertWithPk", routerFromInsertWithPk);
+router.use("/ShowAll", routerFromShowAll);
+
+export { router };
