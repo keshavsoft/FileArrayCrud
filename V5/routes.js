0d48f2251daea5a9caee622aa3fa22d72a61c892
sharath0036 <ksharathbabu2@gmail.com>
2025-05-16 12:40:15 +0530

    Crud in V5

--- V5/routes.js
+++ V5/routes.js
@@ -2,9 +2,11 @@ import express from 'express';
 
 const router = express.Router();
 
+import { router as routerFromSharath } from "./Sharath/routes.js";
 import { router as routerFromSree } from "./Sree/routes.js";
 import { router as routerFromKeshav } from "./Keshav/routes.js";
 
+router.use("/Sharath", routerFromSharath);
 router.use("/Sree", routerFromSree);
 router.use("/Keshav", routerFromKeshav);
 
