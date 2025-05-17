facbc35b2309dfadf0761e70278f5f05fb54dece
Srivalli <gsvalli04@gmail.com>
2025-05-17 12:18:08 +0530

    Testing done on WithTable

--- V5/routes.js
+++ V5/routes.js
@@ -5,9 +5,12 @@ const router = express.Router();
 import { router as routerFromSharath } from "./Sharath/routes.js";
 import { router as routerFromSree } from "./Sree/routes.js";
 import { router as routerFromKeshav } from "./Keshav/routes.js";
+import { router as routerFromSrivalli } from "./Srivalli/routes.js";
 
 router.use("/Sharath", routerFromSharath);
 router.use("/Sree", routerFromSree);
 router.use("/Keshav", routerFromKeshav);
+
+router.use("/Srivalli", routerFromSrivalli);
 
 export { router };
