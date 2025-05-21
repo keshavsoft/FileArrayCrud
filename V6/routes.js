dc3fcd238d8546cbe5f36beab6249b5e797cd351
keshavsoft <nknnkumar@yahoo.com>
2025-05-21 12:16:02 +0530

    v6 varshita

--- V6/routes.js
+++ V6/routes.js
@@ -2,8 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
+import { router as routerFromVarshita } from "./Varshita/routes.js";
 import { router as routerFromKishan } from "./Kishan/routes.js";
 
+router.use("/Varshita", routerFromVarshita);
 router.use("/Kishan", routerFromKishan);
 
 export { router };
