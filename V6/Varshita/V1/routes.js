10444078324bc48d0527c853fcf2c373a6639371
varshitha-2319 <163839907+varshitha-2319@users.noreply.github.com>
2025-05-21 17:15:07 +0530

    Add All Masters

--- V6/Varshita/V1/routes.js
+++ V6/Varshita/V1/routes.js
@@ -2,8 +2,19 @@ import express from 'express';
 
 const router = express.Router();
 
+import { router as routerFromMastersItemTypes } from "./MastersItemTypes/routes.js";
+import { router as routerFromMastersItemService } from "./MastersItemService/routes.js";
+import { router as routerFromMastersItems } from "./MastersItems/routes.js";
+import { router as routerFromMastersItemCategory } from "./MastersItemCategory/routes.js";
+import { router as routerFromMastersCustomers } from "./MastersCustomers/routes.js";
 import { router as routerFromMastersAddOns } from "./MastersAddOns/routes.js";
 
+
+router.use("/MastersItemTypes", routerFromMastersItemTypes);
+router.use("/MastersItemService", routerFromMastersItemService);
+router.use("/MastersItems", routerFromMastersItems);
+router.use("/MastersItemCategory", routerFromMastersItemCategory);
+router.use("/MastersCustomers", routerFromMastersCustomers);
 router.use("/MastersAddOns", routerFromMastersAddOns);
 
 export { router };
